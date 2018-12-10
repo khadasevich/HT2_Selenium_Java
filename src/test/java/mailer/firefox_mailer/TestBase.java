34833fa548f2f1f4e7e1a796c28379fb7b4d7741
khadasevich <khadasevich.aleksey@gmail.com>
2018-12-10 17:49:57 +0300

    Removing basic testing activities to the separate class

--- src/test/java/mailer/firefox_mailer/TestBase.java
+++ src/test/java/mailer/firefox_mailer/TestBase.java
@@ -1,4 +1,28 @@
 package mailer.firefox_mailer;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
 public class TestBase {
+
+    public WebDriver driver;
+
+    @BeforeClass(description = "Start browser and setting waiting variable")
+    public void startBrowser() {
+        driver = new FirefoxDriver();
+        driver.manage().deleteAllCookies();
+//      Open browser on full screen
+        driver.manage().window().maximize();
+//      Open main page
+        new YahooMainPage(driver).openMainPage();
+    }
+
+    @AfterClass(description = "Stop Browser")
+    public void stopBrowser() {
+//      Closes browser after test
+        driver.quit();
+    }
+
 }
