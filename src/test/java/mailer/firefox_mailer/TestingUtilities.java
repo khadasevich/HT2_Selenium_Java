defee1fc4ae8f591c8a9c19e9da26b7f670bfd44
khadasevich <khadasevich.aleksey@gmail.com>
2018-12-10 19:50:28 +0300

    Add utility of old account

--- src/test/java/mailer/firefox_mailer/TestingUtilities.java
+++ src/test/java/mailer/firefox_mailer/TestingUtilities.java
@@ -1,5 +1,6 @@
 package mailer.firefox_mailer;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -13,10 +14,25 @@ public class TestingUtilities extends YahooAbstractClass{
     @FindBy (xpath = "//*[@data-test-id='efv-header']")
     WebElement sntDrft;
 
+    @FindBy (xpath = "//*[@name='targetId']")
+    WebElement clsButton;
+
     public String isElementPresent() {
 //  Checking absence of the element
+        waitClickability(sntDrft);
         String result = sntDrft.getText();
         return result;
     }
+
+
+    public void closeOldAcc() {
+        try {
+            clsButton.click();
+        } catch (NoSuchElementException e) {
+        }
+
+    }
 }
+
+
 
