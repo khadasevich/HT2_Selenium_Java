74667517b6d8d5ea29f1da7ae03289e6d8d37444
khadasevich <khadasevich.aleksey@gmail.com>
2018-12-10 17:47:26 +0300

    Extending class for creation draft

--- src/test/java/mailer/firefox_mailer/CreateDraft.java
+++ src/test/java/mailer/firefox_mailer/CreateDraft.java
@@ -29,6 +29,21 @@ public class CreateDraft extends YahooAbstractClass {
     @FindBy (xpath = "//*[@data-test-id='icon-btn-close']")
     WebElement clssvBtn;
 
+    @FindBy (xpath = "//*[@data-test-folder-name='Draft']")
+    WebElement drftBtn;
+
+    @FindBy (xpath = "//*[@data-test-id='message-subject']")
+    WebElement crtdDraft;
+
+    @FindBy (xpath = "//*[@data-test-id='compose-subject']")
+    WebElement drftSubject;
+
+    @FindBy (xpath = "//*[text()='Wake up, Neo. The matrix has you']")
+    WebElement txbxMsg;
+
+    @FindBy (xpath = "//*[@data-test-id='icon-btn-delete']")
+    WebElement dltBtn;
+
     public CreateDraft draftCreate() {
 //      Method which creates new draft
 //      Let's find compose button and click it
@@ -44,6 +59,44 @@ public class CreateDraft extends YahooAbstractClass {
         txtbxField.sendKeys(DESCRIPTION_TEXT);
 //      Closing and saving as draft
         clssvBtn.click();
+        return this;
+    }
+
+    public CreateDraft openDraft() {
+//      Let's open draft box
+        waitClickability(drftBtn);
+        drftBtn.click();
+        return this;
+    }
+
+
+    public CreateDraft openDraftMsg() {
+//      Let's open draft box
+        openDraft();
+//      Let's open created message
+        waitClickability(crtdDraft);
+        crtdDraft.click();
+        return this;
+    }
+
+
+    public String[] takeParamDraft() {
+        String arResults[] = new String[2];
+//      Method which opens created draft and takes parameters
+        openDraftMsg();
+//      Taking parameters of the draft
+        waitClickability(drftSubject);
+        arResults[0] = drftSubject.getAttribute("value");
+        arResults[1] = txbxMsg.getText();
+        return arResults;
+    }
+
+    public CreateDraft deleteDraft() {
+//      Method which deletes created draft
+        openDraftMsg();
+//      Let's delete draft
+        waitClickability(dltBtn);
+        dltBtn.click();
         return this;
     }
 }
---
+++ src/test/java/mailer/firefox_mailer/TestBase.java
@@ -0,0 +1,4 @@
+package mailer.firefox_mailer;
+
+public class TestBase {
+}
