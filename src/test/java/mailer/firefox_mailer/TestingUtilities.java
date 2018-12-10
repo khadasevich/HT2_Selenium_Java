87ac7b3549c260ad7d31d2d4a446a1ad91b235ed
khadasevich <khadasevich.aleksey@gmail.com>
2018-12-10 17:48:01 +0300

    Creation utility for tests

--- src/test/java/mailer/firefox_mailer/TestingUtilities.java
+++ src/test/java/mailer/firefox_mailer/TestingUtilities.java
@@ -1,4 +1,22 @@
 package mailer.firefox_mailer;
 
-public class TestingUtilities {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class TestingUtilities extends YahooAbstractClass{
+
+    public TestingUtilities (WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy (xpath = "//*[@data-test-id='efv-header']")
+    WebElement sntDrft;
+
+    public String isElementPresent() {
+//  Checking absence of the element
+        String result = sntDrft.getText();
+        return result;
+    }
 }
+
